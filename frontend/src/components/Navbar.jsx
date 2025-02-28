daab089b743e7725c65bcf8f92ab09a8cce08d0b
PradipJaysawal <gabbarsamba948@gmail.com>
2025-02-28 19:59:31 +0545

    pradip

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <div className='flex items-center justify-between py-5 font-medium'>
 
-        <img src={assets.logo} className='w-36' alt="" />
+        <img src={assets.logo} className='w-10' alt="" />
 
         <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
 
