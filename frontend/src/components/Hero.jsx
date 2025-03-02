b85fe6a72084d3c155539ab962daf9f06dd1ecfc
Plug-On <plugon035@gmail.com>
2025-03-02 22:21:04 +0545

    niraj

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -8,9 +8,9 @@ const Hero = () => {
         <div className='text-[#14141]'>
             <div className='flex items-center gap-2'>
                 <p className='w-8 md:w-11 h-[2px] bg-[#414141]'></p>
-                <p className='font-medium text-sm md:text-base'>Our BestSellers</p>
+                <p className='font-medium text-sm md:text-base'>OUR BESTSELLERS</p>
             </div>
-            <h1 className=' pacifico-regular text-3xl sm:py-3 lg:text-5xl leading-relaxed'>Latest Arrivals</h1>
+            <h1 className='pacifico-regular text-3xl sm:py-3 lg:text-5xl leading-relaxed'>Latest Arrivals</h1>
             <div className='flex items-center gap-2'>
                 <p className='font-semibold text-sm md:text-base'>SHOP NOW</p>
                 <p className='w-8 md:w-11 h-[1px] bg-[#414141]'></p>
