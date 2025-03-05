fba56b310b985179593dea9af4cb061a80b2a39d
PradipJaysawal <gabbarsamba948@gmail.com>
2025-03-05 08:57:35 +0545

    pradip

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -13,11 +13,12 @@ const About = () => {
 
       <div className='my-10 flex flex-col md:flex-row gap-16'>
         <img className='w-full md:max-w-[450px]' src={assets.about_img} alt="" />
-        <div className='flex flex-col justify-center gap-6 md:w-2/4 text-gray-600'>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, reprehenderit sapiente asperiores voluptatibus dicta a mollitia voluptates vitae consequatur deserunt porro obcaecati, laudantium ea inventore.</p>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus consequuntur cupiditate tenetur ex nostrum deserunt, explicabo nihil tempore dolorum quia eveniet. Architecto dicta molestias laudantium?</p>
-            <b className='text-gray-800'>Our Mission</b>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea libero expedita vero. Commodi perspiciatis adipisci harum quos deleniti voluptatum necessitatibus quo eaque alias? Quam, distinctio!</p>
+        <div className='flex flex-col justify-center gap-4 md:w-2/4 text-gray-600'>
+            <p className='text-justify'>Welcome to <b>PetZone</b>, your ultimate destination for all things pets! We are dedicated to providing high-quality products that keep your furry, feathered, and scaly friends happy and healthy. From nutritious food to fun toys and cozy accessories, we have everything your pet needs.<br/>  
+            At <b>PetZone</b>, we believe pets are family. That's why we focus on quality, affordability, and convenience, ensuring a seamless shopping experience. Whether you're a proud pet parent or a new owner, we're here to support you with the best products and expert care tips. Shop with us and give your pets the love they deserve!</p>
+            <b className='text-gray-800 font-bold text-xl'>Our Mission</b>
+            <p className='text-justify'>At <b>PetZone</b>, our mission is to provide high-quality pet products that enhance the health, happiness, and well-being of your furry, feathered, and scaly companions. We believe that every pet deserves the best care, which is why we carefully select safe, nutritious, and fun products to support their needs. <br /> 
+            Beyond products, we strive to create a community of pet lovers by offering expert advice, reliable customer support, and a seamless shopping experience. Our goal is to make pet parenting easier, more enjoyable, and filled with love—because pets are family, and they deserve the very best!</p>
         </div>
       </div>
 
@@ -28,15 +29,15 @@ const About = () => {
       <div className='flex flex-col md:flex-row text-sm mb-20'>
         <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
           <b>Quality Assurance:</b>
-          <p className='text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, assumenda quia doloribus aspernatur nam inventore nisi excepturi aliquam ipsam soluta est pariatur itaque dolorum illum!</p>
+          <p className='text-gray-600 text-justify'> At <b>PetZone</b>, we are committed to offering only the best for your pets. Every product in our store is carefully selected to ensure high quality, durability, and safety. From nutritious food to fun toys and essential accessories, we prioritize products that enhance your pet's health and happiness.</p>
         </div>
         <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
           <b>Convenience:</b>
-          <p className='text-gray-600'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas accusamus molestiae, ipsam, saepe possimus voluptas aut ducimus reiciendis inventore doloremque veritatis quo. Cupiditate nihil quo nulla libero, illo laudantium error!</p>
+          <p className='text-gray-600 text-justify'>We make pet shopping effortless with our easy-to-navigate website, secure payment options, and fast delivery service. Whether you're at home or on the go, you can browse, shop, and receive your pet's essentials with just a few clicks—saving you time and effort. </p>
         </div>
         <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
           <b>Exceptional Customer Service:</b>
-          <p className='text-gray-600'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias ducimus odio accusantium ea labore repellendus eum earum soluta voluptatum consequuntur.</p>
+          <p className='text-gray-600 text-justify'>Your satisfaction is our priority! Our dedicated support team is always ready to assist with expert advice, personalized recommendations, and quick resolutions to any queries. We're here to ensure you and your pet have the best shopping experience possible.</p>
         </div>
       </div>
 
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -11,7 +11,7 @@ const onSubmitHandler = async (event) => {
   return (
     <form onSubmit={onSubmitHandler} className='flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800'>
       <div className='inline-flex items-center gap-3 mb-2 mt-10'>
-        <p className='prata-regular text-3xl'>{currentState}</p>   {/* font style not change */}
+        <p className='pacifico-regular text-3xl'>{currentState}</p>   {/* font style not change */}
         <hr className='border-none h-[1.5px] w-8 bg-gray-800' />
       </div>
       {currentState === 'Login' ? '' :<input type="text" className='w-full px-3 py-2 border border-gray-800' placeholder='Name' required/>}
