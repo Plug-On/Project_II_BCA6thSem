57b266e3fafb7ffc89d35c3099e32e1369975ca8
Plug-On <plugon035@gmail.com>
2025-04-03 07:00:29 +0545

    niraj

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
   return (
     <div className='flex items-center justify-between py-5 font-medium'>
 
-        <Link to='/'><img src={assets.logo} className='w-10' alt="" /></Link>
+        <Link to='/'><img src={assets.logo} className=' h-20' alt="" /></Link>
 
         <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
 
