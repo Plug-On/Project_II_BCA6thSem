9ccca7b4108ce704fba9a1980ab3ff0e9b2591fd
PradipJaysawal <gabbarsamba948@gmail.com>
2025-04-13 20:35:50 +0545

    pradip

--- PetZone/frontend/src/App.jsx
+++ PetZone/frontend/src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './components/Home'
 import Shop from './components/Shop'
 import Product from './components/product'
+import Cart from './components/Cart'
+import Checkout from './components/Checkout'
 
 
 function App() {
@@ -15,6 +17,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/shop" element={<Shop />} />
         <Route path="/Product" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} />
       </Routes>
       </BrowserRouter>
     </>
