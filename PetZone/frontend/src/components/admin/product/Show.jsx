69a0ac328b7cbf273fe37b0276fa7f39c19ed42e
Plug-On <plugon035@gmail.com>
2025-04-20 10:31:46 +0545

    niraj

--- PetZone/frontend/src/components/admin/product/Show.jsx
+++ PetZone/frontend/src/components/admin/product/Show.jsx
@@ -110,7 +110,10 @@ const Show = () => {
                                 <tr key={`product-${product.id}`}>
                             <td>{product.id}</td>
                             <td>
-                                <img src={product.image_url} alt="" width={50}/>
+                              {
+                                (product.image_url == "") ? <img src="https://placehold.co/50x50" alt="" /> : <img src={product.image_url} alt="" width={50}/>
+                              }
+                                
                             </td>
                             <td>{product.title}</td>
                             <td>{product.price}</td>
