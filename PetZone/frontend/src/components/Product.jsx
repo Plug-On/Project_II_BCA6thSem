d48a1babb001d9b80fd3eeca35efdb2f97217f04
Plug-On <plugon035@gmail.com>
2025-04-25 16:55:04 +0545

    niraj

--- PetZone/frontend/src/components/Product.jsx
+++ PetZone/frontend/src/components/Product.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import Layout from './common/Layout'
 import { Link, useParams } from 'react-router-dom'
 import { Swiper, SwiperSlide } from 'swiper/react'
@@ -11,6 +11,8 @@ import { Rating } from 'react-simple-star-rating'
 import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
 import { apiUrl } from './common/http';
+import { CartContext } from './context/Cart';
+import { toast } from 'react-toastify';
 
 const Product = () => {
   const [thumbsSwiper, setThumbsSwiper] = useState(null);
@@ -18,7 +20,9 @@ const Product = () => {
   const [product, setProduct] = useState([])
   const [productImages, setProductImages] = useState([])
   const [productSizes, setProductSizes] = useState([])
+  const [sizeSelected, setSizeSelected] = useState(null)
   const params = useParams();
+  const { addToCart } = useContext(CartContext)
 
   const fetchProduct = () => {
     fetch(`${apiUrl}/get-product/${params.id}`, {
@@ -40,6 +44,19 @@ const Product = () => {
       })
   }
 
+  const handleAddToCart  =() =>{
+    if (productSizes.length >0){
+      if (sizeSelected == null){
+        toast.error("Please select a size")
+      } else {
+        addToCart(product , sizeSelected)
+        toast.success("Product added to cart successfully")
+      }
+    } else {
+      addToCart(product , null)
+      toast.success("Product added to cart successfully")
+    }
+  }
   useEffect(() => {
     fetchProduct()
   }, [])
@@ -147,7 +164,10 @@ const Product = () => {
                 {
                     productSizes && productSizes.map(product_size => {
                         return (
-                            <button className='btn btn-size '>{product_size.size.name}</button>
+                            <button
+                            key={`p-size-${product_size.id}`}
+                            onClick={()=> setSizeSelected(product_size.size.name)}
+                            className={`btn btn-size me-2 ${sizeSelected == product_size.size.name ? 'active' : ''}` }>{product_size.size.name}</button>
                         )
                     })
                 }
@@ -155,7 +175,9 @@ const Product = () => {
               </div>
             </div>
             <div className='add-to-cart my-4'>
-              <button className='btn btn-primary text-uppercase'>Add To Cart</button>
+              <button
+              onClick={()=> handleAddToCart()}
+              className='btn btn-primary text-uppercase'>Add To Cart</button>
             </div>
             <hr />
             <div>
---
+++ PetZone/frontend/src/components/context/Cart.jsx
@@ -0,0 +1,124 @@
+import { createContext, useState } from "react";
+
+export const CartContext = createContext();
+export const CartProvider = ({children}) => {
+    const [cartData , setCartData] = useState(JSON.parse(localStorage.getItem('cart')) || [])
+
+    const addToCart = (product, size=null) => {
+        let updatedCart = [...cartData];
+
+        //If cart is empty
+        if (cartData.length == 0){
+            updatedCart.push({
+                id:`${product.id}-${Math.floor(Math.random() * 10000000)}`,
+                product_id: product.id,
+                size: size,
+                title: product.title,
+                price: product.price,
+                qty: 1,
+                image_url: product.image_url
+            })
+        } else {
+            //if size is not empty
+            if(size != null) {
+                const isProductExist= updatedCart.find(item =>
+                    item.product_id == product.id && item.size == size)
+
+                    //if product and size combination exist then increase qty
+                    if ( isProductExist) {
+                        updatedCart = updatedCart.map(item =>
+                             (item.product_id == product.id  && item.size == size)
+                             ? {...item, qty:item.qty+1} 
+                             :item
+                    )
+                    } else {
+                        //If product and size combination not exist then add new item
+                        updatedCart.push({
+                            id:`${product.id}-${Math.floor(Math.random() * 10000000)}`,
+                            product_id: product.id,
+                            size: size,
+                            title: product.title,
+                            price: product.price,
+                            qty: 1,
+                            image_url: product.image_url
+                        })
+                    }
+            } else {
+                //when size is null
+                const isProductExist = updatedCart.find(item =>
+                    item.product_id == product.id
+                )
+
+                if ( isProductExist) {
+                    //when [roduct found in cart increase quantitiy]
+                    updatedCart = updatedCart.map(item =>
+                         (item.product_id == product.id )
+                         ? {...item, qty:item.qty+1} 
+                         :item
+                )
+                } else {
+                    //If product not exist then add new item
+                    updatedCart.push({
+                        id:`${product.id}-${Math.floor(Math.random() * 10000000)}`,
+                        product_id: product.id,
+                        size: size,
+                        title: product.title,
+                        price: product.price,
+                        qty: 1,
+                        image_url: product.image_url
+                    })
+                }
+            }
+        }
+
+        
+
+        setCartData(updatedCart)
+        localStorage.setItem('cart',JSON.stringify(updatedCart))
+    }
+
+    const shipping = () => {
+        return 0;
+    }
+
+    const subTotal = () => {
+        let subtotal = 0;
+        cartData.map (item => {
+            subtotal += item.qty *item.price;
+        })
+        return subtotal;
+    }
+
+    const grandTotal = () => {
+        return subTotal() + shipping();
+    }
+
+
+    const updateCartItem = (itemId , newQty) => {
+        let updatedCart = [...cartData];
+       updatedCart = updatedCart.map(item=> 
+            (item.id == itemId) ? {...item , qty:newQty} :item
+        )
+        setCartData(updatedCart)
+        localStorage.setItem('cart',JSON.stringify(updatedCart))
+    }
+
+    const deleteCartItem = (itemId) => {
+       const newCartData = cartData.filter (item => item.id != itemId)
+       setCartData(newCartData)
+       localStorage.setItem('cart',JSON.stringify(newCartData))
+    }
+    const getQty = () => {
+        let qty = 0;
+        cartData.map(item => {
+            qty += parseInt(item.qty)
+        });
+        return qty;
+    }
+
+    return (
+        <CartContext.Provider value={{addToCart , cartData , grandTotal , subTotal , shipping , updateCartItem , deleteCartItem , getQty}}>
+            {children}
+        </CartContext.Provider>
+    )
+}
